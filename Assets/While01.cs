70eca8efb49d9a59ee5e296b5f5ec41bfe0ceabe
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-04-21 15:36:29 -0300

    Initial commit

---
+++ Assets/While01.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class While01 : MonoBehaviour
+{
+    public int counter;
+    public int counterMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (counter != counterMax && counter > counterMax)
+        {
+            while (counter >= counterMax)
+            {
+                Debug.Log(counter);
+                counter--;
+            }
+
+        }
+        
+        else
+        {
+            Debug.Log("NUMEROS INGRESADOS SON INCORRECTOS");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
